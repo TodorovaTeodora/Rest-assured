fc7a59a54e89c93d766e6c05691d622a45ea765a
TodorovaTeodora <57221159+TodorovaTeodora@users.noreply.github.com>
2023-02-13 09:34:30 +0200

    Update FootballApiConfig.java

--- src/test/java/config/FootballApiConfig.java
+++ src/test/java/config/FootballApiConfig.java
@@ -11,25 +11,21 @@ import org.junit.BeforeClass;
 
 public class FootballApiConfig {
 
-    public static RequestSpecification football_requestSpec;
-    public static ResponseSpecification responseSpecification;
-
     @BeforeClass
     public static void setup() {
-        football_requestSpec = new RequestSpecBuilder()
-                .setBaseUri("http://api.football-data.org")
-                .setBasePath("/v2/")
+        RestAssured.setBaseUri("http://api.football-data.org");
+        RestAssured.setBasePath("/v2/");
+        
+         RestAssured.requestSpecification = new RequestSpecBuilder()
                 .addHeader("X-Auth-Token", "fae8b099875d41f395c58dbb7f35556b")
                 .addHeader("X-Response-Control", "minified")
                 .addFilter(new RequestLoggingFilter())
                 .addFilter(new ResponseLoggingFilter())
                 .build();
 
-        responseSpecification = new ResponseSpecBuilder()
+       RestAssured.responseSpecification = new ResponseSpecBuilder()
                 .expectStatusCode(200)
                 .build();
 
-        RestAssured.requestSpecification = football_requestSpec;
-        RestAssured.responseSpecification = responseSpecification;
     }
 }
